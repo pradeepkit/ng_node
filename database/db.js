4aa28b2fd442220be0337c85ec528db8c5edb1fc
Pradeep <pradeep.kumar@prodist.tech>
2019-12-04 18:57:58 +0530

    config changes

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/ng_node'
+    // db: 'mongodb://localhost:27017/ng_node'
+    db:'pradeep:Divy@1993@ds351628.mlab.com:51628/my-school-portal'
   };
