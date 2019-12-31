a43cd7824babb29bd1a36fa260d6cc48639c64e6
Pradeep <pradeep.kumar@prodist.tech>
2019-12-31 14:27:00 +0530

    master push

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/ng_node'
-    // db: 'mongodb://school:divya1993@ds253408.mlab.com:53408/school_portal'
+    // db: 'mongodb://localhost:27017/ng_node'
+    db: 'mongodb://school:divya1993@ds253408.mlab.com:53408/school_portal'
   };
